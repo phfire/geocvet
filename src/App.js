57b514eab058db8ec598dd50c416b32eff72ddd1
phfire <teo.stoynov@gmail.com>
2018-06-20 00:45:04 +0300

    website structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ScrollableAnchor from "react-scrollable-anchor";
+
+import Layout from "./components/Layout";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<a href='#section1'> Go to section 1 </a>
+				<a href='#section2'> Go to section 2 </a>
+				<Layout>
+					<ScrollableAnchor id={'section1'}>
+						<Header/>
+					</ScrollableAnchor>
+
+					<ScrollableAnchor id={'section2'}>
+						<Footer/>
+					</ScrollableAnchor>
+				</Layout>
+			</div>
+		);
+	}
 }
 
 export default App;

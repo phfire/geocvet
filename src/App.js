9c8959fcba29b38e594f6e71aecd7114779de115
Teodor Stoynov <t.stoynov@gisinv.bg>
2018-06-20 16:01:25 +0300

    projects gallery component added

--- src/App.js
+++ src/App.js
@@ -6,19 +6,29 @@ import ScrollableAnchor from "react-scrollable-anchor";
 import Layout from "./components/Layout";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-
+import About from "./components/About";
+import Directions from "./components/Directions";
+import Projects from "./components/Projects";
 class App extends Component {
 	render() {
 		return (
-			<div>
-				<a href='#section1'> Go to section 1 </a>
-				<a href='#section2'> Go to section 2 </a>
+			<div className="appContainer">
+				<Header/>
+				<a href='#About'>About</a>
+				<a href='#Directions'>Directions</a>
+				<a href='#Footer'>Footer</a>
+				<a href='#Projects'>Projects</a>
 				<Layout>
-					<ScrollableAnchor id={'section1'}>
-						<Header/>
+					<ScrollableAnchor id={'About'}>
+						<About/>
 					</ScrollableAnchor>
-
-					<ScrollableAnchor id={'section2'}>
+					<ScrollableAnchor id={'Directions'}>
+						<Directions/>
+					</ScrollableAnchor>
+					<ScrollableAnchor id={'Projects'}>
+						<Projects/>
+					</ScrollableAnchor>
+					<ScrollableAnchor id={'Footer'}>
 						<Footer/>
 					</ScrollableAnchor>
 				</Layout>
---
+++ src/components/Projects.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ProjectsGallery from "./ProjectsGallery";
+
+class Projects extends Component{
+    render(){
+        return(
+            <div>
+                <ProjectsGallery/>
+            </div>
+        )
+    }
+} 
+export default Projects;
